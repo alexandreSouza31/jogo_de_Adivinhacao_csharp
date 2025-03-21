ce38181e84653fb65a29087d36ce91565a3b595d
alexandreSouza31 <alexandre31_m@hotmail.com>
2025-03-21 15:02:43 -0300

    Armazenará e exibirá números já digitados

--- jogoDeAdivinhacao.ConsoleApp/Program.cs
+++ jogoDeAdivinhacao.ConsoleApp/Program.cs
@@ -3,42 +3,55 @@
     static void Main(string[] args)
     {
 
+        string[] numerosJaDigitados= new string[100];
+        int contador = 0;
+
         while (true)
         {
+            Console.Clear();
             Console.WriteLine("-------------------------------------");
             Console.WriteLine("        Jogo de Adivinhação");
             Console.WriteLine("-------------------------------------");
 
             Random geradorDeNumeros = new Random();
             int numeroSorteado = geradorDeNumeros.Next(1, 21);
-            Console.WriteLine(numeroSorteado);
 
             Console.Write("Digite um número entre 1 e 20: ", " \n");
-            int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+            int numeroDigitadoInput = Convert.ToInt32(Console.ReadLine());
 
-            if (numeroDigitado == numeroSorteado)
+            string historicoDigitados=numerosJaDigitados[contador] = Convert.ToString(numeroDigitadoInput);
+
+            if (numeroDigitadoInput == numeroSorteado)
             {
                 Console.WriteLine($"Parabéns, você acertou! O número sorteado foi {numeroSorteado}!");
 
             }
-            else if (numeroDigitado < numeroSorteado)
+            else if (numeroDigitadoInput < numeroSorteado)
             {
-                Console.WriteLine($"\n{numeroDigitado} é menor que o número sorteado!", "\n");
+                Console.WriteLine($"\n{numeroDigitadoInput} é menor que o número sorteado!", "\n");
             }
-            else if (numeroDigitado > numeroSorteado)
+            else if (numeroDigitadoInput > numeroSorteado)
             {
-                Console.WriteLine($"\n{numeroDigitado} é maior que o número sorteado!", "\n");
+                Console.WriteLine($"\n{numeroDigitadoInput} é maior que o número sorteado!", "\n");
             }
 
+            contador++;
 
-            Console.Write("Deseja jogar novamente?[S/N]: ");
+            Console.WriteLine("..............................................................");
+
+            string historicoDigitadosJoin = string.Join(", ", numerosJaDigitados.Where(n => n != null));
+            Console.WriteLine($"Histórico de digitados: {historicoDigitadosJoin}", "\t");
+           
+            Console.WriteLine("..............................................................");
+
+            Console.Write("\nDeseja jogar novamente?[S/N]: ");
             string jogarNovamente = Console.ReadLine().ToUpper();
 
             if (jogarNovamente != "S")
             {
                 break;
             }
-
+            
         }
     }
 }
